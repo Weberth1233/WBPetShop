fd8948519fc2690dc8525535a9574710d91af483
Weberth1233 <weberth20-19@hotmail.com>
2020-11-18 15:16:10 -0300

    Adicionando funcionário na sessão e template, utilizando o security.

---
+++ PetShop/src/br/unitins/petshop/controller/TemplateController.java
@@ -0,0 +1,34 @@
+package br.unitins.petshop.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import br.unitins.petshop.application.Session;
+import br.unitins.petshop.model.Funcionario;
+
+@Named
+@ViewScoped
+public class TemplateController implements Serializable{
+
+	private static final long serialVersionUID = -3625157056539958909L;
+	private Funcionario funcionarioLogado = null;
+	
+	public TemplateController() {}
+	
+	public Funcionario getFuncionarioLogado() {
+		if(funcionarioLogado == null) 
+			funcionarioLogado = (Funcionario) Session.getInstance().getAttribute("funcionarioLogado");
+		return funcionarioLogado;
+	}
+
+	public void setFuncionarioLogado(Funcionario funcionarioLogado) {
+		this.funcionarioLogado = funcionarioLogado;
+	}
+	
+	public String encerrarSessao() {
+		Session.getInstance().invalidateSession();
+		return "login.xhtml?faces-redirect=true";
+	}
+}
---
+++ PetShop/src/br/unitins/petshop/filter/SecurityFilter.java
@@ -0,0 +1,59 @@
+package br.unitins.petshop.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.unitins.petshop.model.Funcionario;
+
+
+/*Assim vai percorrer todas as paginas no filter*/
+@WebFilter(filterName = "SecurityFilter", urlPatterns = {"/faces/*"})
+public class SecurityFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		System.out.println("SecurityFilter Iniciado.");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		/*chain.doFilter(request, response);
+		return;*/
+		
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		String url = httpServletRequest.getRequestURI();
+		System.out.println(url);
+		if(url.equals("/PetShop/faces/login.xhtml")) {
+			
+			chain.doFilter(request, response);
+			return;
+		}
+		HttpSession session = httpServletRequest.getSession(false);
+
+		Funcionario funcionario = null;
+		if(session != null) {
+			funcionario = (Funcionario) session.getAttribute("funcionarioLogado");
+		}
+		if(funcionario == null) {
+			((HttpServletResponse) response).sendRedirect("/PetShop/faces/login.xhtml");
+		}else {
+			chain.doFilter(request, response);
+			return;
+		}
+	}
+	@Override
+	public void destroy() {
+	}
+}
