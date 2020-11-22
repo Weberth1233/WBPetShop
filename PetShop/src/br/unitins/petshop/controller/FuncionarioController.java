3458b7fb3629f4e356580d7e42c44f442c8d31d8
Weberth1233 <weberth20-19@hotmail.com>
2020-11-22 08:21:55 -0300

    Salvando imagem junto com os dados.😂

--- PetShop/src/br/unitins/petshop/controller/FuncionarioController.java
+++ PetShop/src/br/unitins/petshop/controller/FuncionarioController.java
@@ -1,17 +1,23 @@
 package br.unitins.petshop.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.file.UploadedFile;
+
 import br.unitins.petshop.application.RepositoryException;
+import br.unitins.petshop.application.Session;
 import br.unitins.petshop.application.Util;
+
 import br.unitins.petshop.model.Funcionario;
 import br.unitins.petshop.model.Servico;
 import br.unitins.petshop.model.TipoFuncionario;
-import br.unitins.petshop.repository.ExameRepository;
 import br.unitins.petshop.repository.FuncionarioRepository;
 import br.unitins.petshop.repository.ServicoRepository;
 @Named
@@ -21,13 +27,36 @@ public class FuncionarioController extends Controller<Funcionario>{
 	private static final long serialVersionUID = 9011861114364637235L;
 	private List<Funcionario>listaFuncionario;
 	private String buscar;
+
+	private InputStream fotoInputStream = null;
+	private String nomeFoto = null;
+
 	@Override
 	public Funcionario getEntity() {
 		if(entity == null) {
-			entity = new Funcionario();
+			entity = (Funcionario) Session.getInstance().getAttribute("dadosFunc");
 			entity.setServico(new Servico());
 		}
 		return entity;
+	}
+	
+	@Override
+	public void salvar() {
+		FuncionarioRepository repo = new FuncionarioRepository();
+		try {
+			repo.beginTransaction();
+			setEntity(repo.salvar(getEntity()));
+			if (! Util.saveImageUsuario(fotoInputStream, "png", getEntity().getId())) 
+				throw new RepositoryException("Erro ao salvar. Não foi possível salvar a imagem do usuário.");
+			repo.commitTransaction();
+			Util.addInfoMessage("Operação realizada com sucesso.");
+			limpar();
+		} catch (RepositoryException e) {
+			repo.rollbackTransaction();
+			System.out.println("Erro ao salvar.");
+			e.printStackTrace();
+			Util.addErrorMessage(e.getMessage());
+		}
 	}
 	/*public void pesquisar() {
 		FuncionarioRepository repo = new FuncionarioRepository();
@@ -54,6 +83,33 @@ public class FuncionarioController extends Controller<Funcionario>{
 		return TipoFuncionario.values();
 	}
 	
+	public void upload(FileUploadEvent event) {
+		UploadedFile uploadFile = event.getFile();
+		System.out.println("nome arquivo: " + uploadFile.getFileName());
+		System.out.println("tipo: " + uploadFile.getContentType());
+		System.out.println("tamanho: " + uploadFile.getSize());
+
+		if (uploadFile.getContentType().equals("image/png")) {
+			try {
+				fotoInputStream = uploadFile.getInputStream();
+				nomeFoto = uploadFile.getFileName();
+				System.out.println("inputStream: " + uploadFile.getInputStream().toString());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Util.addInfoMessage("Upload realizado com sucesso.");
+		} else {
+			Util.addErrorMessage("O tipo da image deve ser png.");
+		}
+
+	}
+	public String getNomeFoto() {
+		if (nomeFoto == null) 
+			return "Selecione uma foto ...";
+		return "Arquivo: "+ nomeFoto + " (Clique para alterar a foto...)";
+	}
+	
 	public List<Servico>completeMarca(String query) {
 		ServicoRepository repo = new ServicoRepository();
 		try {
@@ -63,7 +119,7 @@ public class FuncionarioController extends Controller<Funcionario>{
 			return new ArrayList<Servico>();
 		}
 	}
- 	public List<Funcionario> getListaFuncionario() {
+	public List<Funcionario> getListaFuncionario() {
 		if(listaFuncionario==null)
 			listaFuncionario= new ArrayList<Funcionario>();
 		return listaFuncionario;
@@ -80,5 +136,13 @@ public class FuncionarioController extends Controller<Funcionario>{
 	public void setBuscar(String buscar) {
 		this.buscar = buscar;
 	}
-	
+	public String adicionarNovo() {
+		Session.getInstance().setAttribute("dadosFunc", new Funcionario());
+		return "cadfuncionario.xhtml?faces-redirect=true";
+	}
+	public String editarFuncionario(Funcionario funcionario) {
+		setEntity(funcionario);
+		Session.getInstance().setAttribute("dadosFunc", getEntity());
+		return "cadfuncionario.xhtml?faces-redirect=true";
+	}
 }
