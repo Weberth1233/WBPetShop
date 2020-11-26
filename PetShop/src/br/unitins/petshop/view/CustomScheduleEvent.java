f69132ee06998bc72511172b938e554f3a66a313
Weberth1233 <weberth20-19@hotmail.com>
2020-11-26 12:48:44 -0300

    Customizando a tabela para receber os dados do agendamento 😉

---
+++ PetShop/src/br/unitins/petshop/view/CustomScheduleEvent.java
@@ -0,0 +1,161 @@
+package br.unitins.petshop.view;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleRenderingMode;
+
+import br.unitins.petshop.model.Cliente;
+
+public class CustomScheduleEvent implements ScheduleEvent {
+
+    private Integer id;
+    private String title;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String styleClass;
+    private Object data;
+    private String url;
+    private String description;
+    private Cliente cliente; 
+    private boolean allDay;
+    private boolean editable;
+
+    public CustomScheduleEvent() {
+    }
+
+    public CustomScheduleEvent(String title, LocalDateTime start, LocalDateTime end) {
+        this.title = title;
+        this.startDate = start;
+        this.endDate = end;
+        this.allDay = allDay;
+        this.data = data;
+    }
+
+    public CustomScheduleEvent(String title, LocalDateTime start, LocalDateTime end, String styleClass, boolean allDay, Object data) {
+        this.title = title;
+        this.startDate = start;
+        this.endDate = end;
+        this.styleClass = styleClass;
+        this.allDay = allDay;
+        this.data = data;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public boolean isAllDay() {
+        return allDay;
+    }
+
+    public void setAllDay(boolean allDay) {
+        this.allDay = allDay;
+    }
+
+    @Override
+    public String getStyleClass() {
+        return styleClass;
+    }
+
+    public void setStyleClass(String styleClass) {
+        this.styleClass = styleClass;
+    }
+
+    @Override
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    @Override
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean isEditable() {
+        return editable;
+    }
+
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
+
+	@Override
+	public String getGroupId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isOverlapAllowed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ScheduleRenderingMode getRenderingMode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map getDynamicProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setId(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+}
