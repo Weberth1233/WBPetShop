e12944d7acdb3b09dd9598e811b791edca35e824
Weberth1233 <weberth20-19@hotmail.com>
2020-11-27 18:27:16 -0300

    Organizando classe funcionário 😂

--- PetShop/src/br/unitins/petshop/model/Funcionario.java
+++ PetShop/src/br/unitins/petshop/model/Funcionario.java
@@ -1,12 +1,14 @@
 package br.unitins.petshop.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
