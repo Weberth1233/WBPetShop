9b707360d0232cbcc61ad3f03e8124a0b864de7f
Weberth1233 <weberth20-19@hotmail.com>
2020-11-28 22:14:26 -0300

    Relacionamento entre Agendamento de Serviço com a classe Serviço👀

--- PetShop/src/br/unitins/petshop/model/AgendamentoServico.java
+++ PetShop/src/br/unitins/petshop/model/AgendamentoServico.java
@@ -1,15 +1,24 @@
 package br.unitins.petshop.model;
-
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class AgendamentoServico extends Agendamento{
 	private Float valorServico;
+	
+	@ManyToOne
+	private Servico servico;
 	
 	public Float getValorServico() {
 		return valorServico;
 	}
 	public void setValorServico(Float valorServico) {
 		this.valorServico = valorServico;
+	}
+	public Servico getServico() {
+		return servico;
+	}
+	public void setServico(Servico servico) {
+		this.servico = servico;
 	}
 }
