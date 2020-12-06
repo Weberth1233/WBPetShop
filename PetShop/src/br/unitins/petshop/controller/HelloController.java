2082dcfda360ee01ca8807cd20d545763e282e53
Weberth1233 <weberth20-19@hotmail.com>
2020-12-06 02:14:47 -0300

    Testando lista de agendamentos de veterinário 💕

--- PetShop/src/br/unitins/petshop/controller/HelloController.java
+++ PetShop/src/br/unitins/petshop/controller/HelloController.java
@@ -1,14 +1,37 @@
 package br.unitins.petshop.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+
+import br.unitins.petshop.application.RepositoryException;
+import br.unitins.petshop.model.AgendamentoConsulta;
+import br.unitins.petshop.repository.AgendaConsultaRepository;
 
 @Named
 @ViewScoped
 public class HelloController implements Serializable {
 
 	private static final long serialVersionUID = 1935775511554974550L;
+	private List<AgendamentoConsulta> lista;
+	
+	public HelloController() {
+		AgendaConsultaRepository repo= new AgendaConsultaRepository();
+		try {
+			lista = repo.findListVet(2);
+		} catch (RepositoryException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<AgendamentoConsulta> getLista() {
+		return lista;
+	}
+	public void setLista(List<AgendamentoConsulta> lista) {
+		this.lista = lista;
+	}
+	
 	
 }
